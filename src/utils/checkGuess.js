b9241aafc7cf1a1118e546360cd668e4bde68f47
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-03-27 14:45:29 +0100

    Logic to check a new guess complete

--- src/utils/checkGuess.js
+++ src/utils/checkGuess.js
@@ -1,21 +1,35 @@
 import IsDictionaryWord from "./isDictionaryWord";
 
-const CheckGuess = (data,solution,letterColor) => {
+const CheckGuess = (data, solution, letterColor) => {
     console.log("CHECK");
     console.log(`SOLUTION: ${solution}`);
-    
+
     let temp = [...solution];
-    let guess = data.guessLetters[data.row];
+    let temp1 = data.guessLetters[data.row];
+    let guess = [...temp1];
     console.log("Solution: " + temp);
     console.log("Guess: " + guess);
 
-    let diff=[false,false,false,false,false];
-    for(let i = 0; i < 5; i++){
-        diff[i] = (temp[i] === guess[i]) ? "green" : "red";
+    let diff = [false, false, false, false, false];
+    for (let i = 0; i < 5; i++) {
+        if (temp[i] === guess[i]) {
+            diff[i] = "green";
+            temp[i] = "";
+        } 
     }
-    console.log("Diff: " + diff);
-    letterColor[data.row]=diff;
-    return letterColor;
+    for (let i = 0; i < 5; i++) {
+        if (diff[i] !== "green") {
+            if (temp.includes(guess[i])) {
+                diff[i] = "GoldenRod";
+            } else {
+                diff[i] = "grey";
+            }
+        }  
+    }
+
+console.log("Diff: " + diff);
+letterColor[data.row] = diff;
+return letterColor;
     /***************************
      * Is the word the guessed word?
      * 
